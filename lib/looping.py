0712ad0aaddbf3fa59d359ae2eaed6c512afd119
Ali Sheikh Zubeir Noor <ali.noor1@student.moringaschool.com>
2025-05-09 20:20:53 +0300

    Adds changes

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for count in range (10, 0 , -1):
+        print (count)
+        print("Happy New Year!")
+
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [int * int for int in int_list]
+    return int_list
+    
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0 :
+            print("FizzBuzz")
+        elif number % 3 == 0 :
+            print ("Fizz")
+        elif number % 5 == 0 :
+            print ("Buzz")
+        else :
+            print(number)
+        
+
